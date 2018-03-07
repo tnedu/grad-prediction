d3c06235ba6eadbbc8ae099f2ccddafbfedd47a2
Alexander Poon <alex.poon@tn.gov>
2018-03-07 12:22:03 -0600

    implement changes from Jared

--- models.R
+++ models.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(zeallot)
 library(caret)
 library(doParallel)
 
@@ -8,81 +9,49 @@ registerDoParallel(cl)
 
 prediction_data_8 <- read_csv("data/prediction_data_8.csv") %>%
     filter(!is.na(scale_score_mt) & !is.na(scale_score_rd) & !is.na(ready_grad)) %>%
-    mutate(ready_grad = factor(ready_grad))
+    mutate(ready_grad = factor(if_else(ready_grad == 1, "ready", "not_ready"))) %>%
+    select(cohort, ready_grad, n_absences, 
+        E, I, R, S, assault, weapons, theft_vandalism, sexual_assault_harassment, drugs_alcohol,
+        threat, school_rules, bullying, fighting,
+        scale_score_mt, scale_score_mt_sq, scale_score_rd, scale_score_rd_sq,
+        school_scale_score_mt, school_scale_score_rd, school_chronic_abs)
 
-train <- filter(prediction_data_8, cohort == 2012)
-test <- filter(prediction_data_8, cohort == 2011)
+c(train, test) %<-% (split(prediction_data_8, prediction_data_8$cohort) %>% map(as.data.frame))
 
-ctrl <- trainControl(method = "cv", number = 10)
+train_y <- train[, 2]
+train_x <- train[, 3:ncol(train)]
+test_y <- test[, 2]
+test_x <- test[, 3:ncol(test)]
+
+# Preprocess by centering, scaling, and removing zero-variance predictors
+train_preprocess <- preProcess(train_x, method = c("scale", "center", "zv"))
+
+train_x <- predict(train_preprocess, train_x)
+test_x <- predict(train_preprocess, test_x)
+
+train_controls <- trainControl(
+    summaryFunction = twoClassSummary,  # look at classification stats
+    method = "repeatedcv",
+    repeats = 3,                        # repeated cv is pretty standard
+    savePredictions = "final",          # only need final model predictions
+    classProbs = TRUE,                  # want to generate class probabilities for AUC
+    search = "random")                  # this is a default, may want to specify search grid later
 
 # A first GLM model
-model_logistic <- train(ready_grad ~ n_absences +
-        E + I + R + S + assault + weapons + theft_vandalism + sexual_assault_harassment + drugs_alcohol +
-        threat + school_rules + bullying + fighting +
-        scale_score_mt + scale_score_mt_sq + scale_score_rd + scale_score_rd_sq +
-        school_scale_score_mt + school_scale_score_rd + school_chronic_abs,
-    data = train, method = "glm", family = "binomial", trControl = ctrl)
+model_logistic <- train(x = train_x, y = train_y,
+    method = "glm", metric = "ROC", family = "binomial", trControl = train_controls)
 
 model_logistic
 
 # Looking at the coefficients
-coef(model_logistic$finalModel)
-varImp(model_logistic)
+summary(model_logistic$finalModel)
+ggplot(varImp(model_logistic)) + theme_minimal()
 
 # Predicting on test set
-test$probs_logistic <- predict(model_logistic, test, type = "prob")$`1`
+test$probs_logistic <- predict(model_logistic, test_x, type = "prob")$ready
 ggplot(test, aes(x = ready_grad, y = probs_logistic)) + geom_jitter(alpha = 0.1) + theme_minimal()
 
-test$preds_logistic <- predict(model_logistic, test)
-
-confusionMatrix(test$preds_logistic, test$ready_grad)
-
-# Random Forest
-model_rf <- train(ready_grad ~ n_absences +
-        E + I + R + S + assault + weapons + theft_vandalism + sexual_assault_harassment + drugs_alcohol +
-        threat + school_rules + bullying + fighting +
-        scale_score_mt + scale_score_mt_sq + scale_score_rd + scale_score_rd_sq +
-        school_scale_score_mt + school_scale_score_rd + school_chronic_abs,
-    data = train, method = "ranger", trControl = ctrl)
-
-test$preds_rf <- predict(model_rf, test)
-
-confusionMatrix(test$preds_rf, test$ready_grad)
-
-# SVM with a Polynomial Kernel
-model_svm <- train(ready_grad ~ n_absences +
-        E + I + R + S + assault + weapons + theft_vandalism + sexual_assault_harassment + drugs_alcohol +
-        threat + school_rules + bullying + fighting +
-        scale_score_mt + scale_score_mt_sq + scale_score_rd + scale_score_rd_sq +
-        school_scale_score_mt + school_scale_score_rd + school_chronic_abs,
-    data = train, method = "svmPoly", trControl = ctrl)
-
-test$preds_svm <- predict(model_svm, test)
-
-confusionMatrix(test$preds_svm, test$ready_grad)
-
-# SVM with a RBF Kernel
-model_rbf <- train(ready_grad ~ n_absences +
-        E + I + R + S + assault + weapons + theft_vandalism + sexual_assault_harassment + drugs_alcohol +
-        threat + school_rules + bullying + fighting +
-        scale_score_mt + scale_score_mt_sq + scale_score_rd + scale_score_rd_sq +
-        school_scale_score_mt + school_scale_score_rd + school_chronic_abs,
-    data = train, method = "svmRadial", trControl = ctrl)
-
-test$preds_rbf <- predict(model_rbf, test)
-
-confusionMatrix(test$preds_rbf, test$ready_grad)
-
-# K Nearest Neighbors
-model_knn <- train(ready_grad ~ n_absences +
-        E + I + R + S + assault + weapons + theft_vandalism + sexual_assault_harassment + drugs_alcohol +
-        threat + school_rules + bullying + fighting +
-        scale_score_mt + scale_score_mt_sq + scale_score_rd + scale_score_rd_sq +
-        school_scale_score_mt + school_scale_score_rd + school_chronic_abs,
-    data = train, method = "knn", trControl = ctrl, tuneLength = 25)
-
-test$preds_knn <- predict(model_knn, test)
-
-confusionMatrix(test$preds_knn, test$ready_grad)
+preds_logistic <- predict(model_logistic, test_x)
+confusionMatrix(preds_logistic, test_y)
 
 stopCluster(cl)
