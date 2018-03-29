21e11dce36209f41b23bc87f07b9a1ac7f42d7aa
Alexander Poon <alex.poon@tn.gov>
2018-03-29 17:13:55 -0500

    add two more models and pass reference to `train`

--- models.R
+++ models.R
@@ -3,10 +3,6 @@ library(zeallot)
 library(caret)
 library(doParallel)
 
-# Use parallel processing
-cl <- makeCluster(8)
-registerDoParallel(cl)
-
 prediction_data_8 <- read_csv("data/prediction_data_8.csv") %>%
     filter(!is.na(scale_score_mt) & !is.na(scale_score_rd) & !is.na(ready_grad)) %>%
     mutate(ready_grad = factor(if_else(ready_grad == 1, "ready", "not_ready"))) %>%
@@ -16,12 +12,12 @@ prediction_data_8 <- read_csv("data/prediction_data_8.csv") %>%
         scale_score_mt, scale_score_mt_sq, scale_score_rd, scale_score_rd_sq,
         school_scale_score_mt, school_scale_score_rd, school_chronic_abs)
 
-c(train, test) %<-% (split(prediction_data_8, prediction_data_8$cohort) %>% map(as.data.frame))
+c(train_set, test_set) %<-% (split(prediction_data_8, prediction_data_8$cohort) %>% map(as.data.frame))
 
-train_y <- train[, 2]
-train_x <- train[, 3:ncol(train)]
-test_y <- test[, 2]
-test_x <- test[, 3:ncol(test)]
+train_y <- train_set[, 2]
+train_x <- train_set[, 3:ncol(train_set)]
+test_y <- test_set[, 2]
+test_x <- test_set[, 3:ncol(test_set)]
 
 # Preprocess by centering, scaling, and removing zero-variance predictors
 train_preprocess <- preProcess(train_x, method = c("scale", "center", "zv"))
@@ -41,17 +37,47 @@ train_controls <- trainControl(
 model_logistic <- train(x = train_x, y = train_y,
     method = "glm", metric = "ROC", family = "binomial", trControl = train_controls)
 
-model_logistic
-
 # Looking at the coefficients
 summary(model_logistic$finalModel)
 ggplot(varImp(model_logistic)) + theme_minimal()
 
 # Predicting on test set
-test$probs_logistic <- predict(model_logistic, test_x, type = "prob")$ready
-ggplot(test, aes(x = ready_grad, y = probs_logistic)) + geom_jitter(alpha = 0.1) + theme_minimal()
+test_set$probs_logistic <- predict(model_logistic, test_x, type = "prob")$ready
+ggplot(test_set, aes(x = ready_grad, y = probs_logistic)) + geom_jitter(alpha = 0.1) + theme_minimal()
 
 preds_logistic <- predict(model_logistic, test_x)
 confusionMatrix(preds_logistic, test_y)
 
-stopCluster(cl)
+# Other methods
+methods_list <- c("gbm", "rpart", "rlda", "gpls", "nnet", "xgbLinear", "xgbTree")
+
+model_list <- vector(length(methods_list), mode = "list")
+names(model_list) <- methods_list
+
+global_args <- list(x = quote(train_x), y = quote(train_y),
+    metric = "AUC", tuneLength = 50,
+    trControl = train_controls)
+
+fit_list <- map(names(model_list), function(m) {
+
+    registerDoParallel(cores = 8) # Use parallel processing
+
+    print(m) # Update you on where things are running
+
+    model_args <- c(global_args, method = m) # create a new argument object, use method here
+    model <- tryCatch(do.call(train, model_args), error = function(e) NULL)
+
+    write_rds(model, paste0("models/grade_8_", m, ".rds"))
+
+    message("Model method: ", m, " completed.")
+
+    stopImplicitCluster()
+
+    return(model)
+
+    }
+)
+
+# A safe version of predict
+safely_predict <- safely(predict)
+predict_list <- map(fit_list, safely_predict, test_x)
