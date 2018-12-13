b4f02d7c8ccbe82c31645c69425e6bf647d846b0
Alexander Poon <alex.poon@tn.gov>
2018-12-13 15:41:41 -0600

    train and evaluate models for grades 6 and 7

--- R/1. model.R
+++ R/1. model.R
@@ -4,111 +4,118 @@ library(caret)
 library(doParallel)
 library(yardstick)
 
-prediction_data_8 <- read_csv("data/prediction_data_8.csv",
-        col_types = "idciiiiiiiiiiiiiiiiiiiidddcciiiiiiiddddddd") %>%
-    filter(!is.na(scale_score_mt) & !is.na(scale_score_rd) & !is.na(ready_grad)) %>%
-    mutate(ready_grad = factor(if_else(ready_grad == 1, "ready", "not_ready"))) %>%
-    select(cohort, ready_grad, n_absences, enrollments,
-        E, I, R, S, assault, weapons, theft_vandalism, sexual_assault_harassment, drugs_alcohol,
-        threat, school_rules, bullying, fighting,
-        scale_score_mt, scale_score_mt_sq, scale_score_rd, scale_score_rd_sq,
-        school_scale_score_mt, school_scale_score_rd, school_chronic_abs)
+for (grade in 6:8) {
 
-predictors <- c("n_absences", "enrollments", "E", "I", "R", "S", "assault", "weapons",
-    "theft_vandalism", "sexual_assault_harassment", "drugs_alcohol", "threat", "school_rules", "bullying", "fighting",
-    "scale_score_mt", "scale_score_mt_sq", "scale_score_rd", "scale_score_rd_sq",
-    "school_scale_score_mt", "school_scale_score_rd", "school_chronic_abs")
+    path <- paste0("data/prediction_data_", grade, ".csv")
 
-c(train_set, test_set) %<-% (split(prediction_data_8, prediction_data_8$cohort) %>% map(as.data.frame))
+    prediction_data <- read_csv(path, col_types = "idciiiiiiiiiiiiiiiiiiiidddcciiiiiiiddddddd") %>%
+        filter(!is.na(scale_score_mt) & !is.na(scale_score_rd) & !is.na(ready_grad)) %>%
+        mutate(ready_grad = factor(if_else(ready_grad == 1, "ready", "not_ready"))) %>%
+        select(cohort, ready_grad, n_absences, enrollments,
+            E, I, R, S, assault, weapons, theft_vandalism, sexual_assault_harassment, drugs_alcohol,
+            threat, school_rules, bullying, fighting,
+            scale_score_mt, scale_score_mt_sq, scale_score_rd, scale_score_rd_sq,
+            school_scale_score_mt, school_scale_score_rd, school_chronic_abs)
 
-train_y <- train_set$ready_grad
-train_x <- train_set[predictors]
-test_y <- test_set$ready_grad
-test_x <- test_set[predictors]
+    predictors <- c("n_absences", "enrollments", "E", "I", "R", "S", "assault", "weapons",
+        "theft_vandalism", "sexual_assault_harassment", "drugs_alcohol", "threat", "school_rules", "bullying", "fighting",
+        "scale_score_mt", "scale_score_mt_sq", "scale_score_rd", "scale_score_rd_sq",
+        "school_scale_score_mt", "school_scale_score_rd", "school_chronic_abs")
 
-# Preprocess by centering, scaling, and removing zero-variance predictors
-train_preprocess <- preProcess(train_x, method = c("scale", "center", "zv"))
+    c(train_set, test_set) %<-% (split(prediction_data, prediction_data$cohort) %>% map(as.data.frame))
 
-train_x <- predict(train_preprocess, train_x)
-test_x <- predict(train_preprocess, test_x)
+    train_y <- train_set$ready_grad
+    train_x <- train_set[predictors]
+    test_y <- test_set$ready_grad
+    test_x <- test_set[predictors]
+    
+    # Preprocess by centering, scaling, and removing zero-variance predictors
+    train_preprocess <- preProcess(train_x, method = c("scale", "center", "zv"))
+    
+    train_x <- predict(train_preprocess, train_x)
+    test_x <- predict(train_preprocess, test_x)
+    
+    train_controls <- trainControl(
+        summaryFunction = twoClassSummary,  # look at classification stats
+        method = "repeatedcv",
+        repeats = 3,                        # repeated cv is pretty standard
+        savePredictions = "final",          # only need final model predictions
+        classProbs = TRUE,                  # want to generate class probabilities for AUC
+        search = "random")                  # this is a default, may want to specify search grid later
 
-train_controls <- trainControl(
-    summaryFunction = twoClassSummary,  # look at classification stats
-    method = "repeatedcv",
-    repeats = 3,                        # repeated cv is pretty standard
-    savePredictions = "final",          # only need final model predictions
-    classProbs = TRUE,                  # want to generate class probabilities for AUC
-    search = "random")                  # this is a default, may want to specify search grid later
+    # A first GLM model
+    model_logistic <- train(x = train_x, y = train_y,
+        method = "glm", metric = "ROC", family = "binomial", trControl = train_controls)
 
-# A first GLM model
-model_logistic <- train(x = train_x, y = train_y,
-    method = "glm", metric = "ROC", family = "binomial", trControl = train_controls)
+    # Looking at the coefficients
+    summary(model_logistic$finalModel)
+    ggplot(varImp(model_logistic)) + theme_minimal()
 
-# Looking at the coefficients
-summary(model_logistic$finalModel)
-ggplot(varImp(model_logistic)) + theme_minimal()
+    # Predicting on test set
+    test_set$probs_logistic <- predict(model_logistic, test_x, type = "prob")$ready
+    ggplot(test_set, aes(x = ready_grad, y = probs_logistic)) + geom_jitter(alpha = 0.1) + theme_minimal()
 
-# Predicting on test set
-test_set$probs_logistic <- predict(model_logistic, test_x, type = "prob")$ready
-ggplot(test_set, aes(x = ready_grad, y = probs_logistic)) + geom_jitter(alpha = 0.1) + theme_minimal()
+    preds_logistic <- predict(model_logistic, test_x)
+    confusionMatrix(preds_logistic, test_y)
 
-preds_logistic <- predict(model_logistic, test_x)
-confusionMatrix(preds_logistic, test_y)
+    write_rds(model_logistic, paste0("models/grade_", grade, "_glm.rds"))
+    
+    # Other methods
+    methods_list <- c("gbm", "rpart", "rlda", "nnet", "xgbLinear", "xgbTree")
+    
+    model_list <- vector(length(methods_list), mode = "list")
+    names(model_list) <- methods_list
+    
+    global_args <- list(x = quote(train_x), y = quote(train_y),
+        metric = "AUC", tuneLength = 50,
+        trControl = train_controls)
+    
+    fit_list <- map(names(model_list), function(m) {
+    
+        registerDoParallel(cores = 8) # Use parallel processing
+    
+        print(m) # Update you on where things are running
+        
+        model_args <- c(global_args, method = m) # create a new argument object, use method here
+        model <- tryCatch(do.call(train, model_args), error = function(e) NULL)
 
-write_rds(model_logistic, "models/grade_8_glm.rds")
+        write_rds(model, paste0("models/grade_", grade, "_", m, ".rds"))
 
-# Other methods
-methods_list <- c("gbm", "rpart", "rlda", "nnet", "xgbLinear", "xgbTree")
+        message("Model method: ", m, " completed.")
 
-model_list <- vector(length(methods_list), mode = "list")
-names(model_list) <- methods_list
+        stopImplicitCluster()
 
-global_args <- list(x = quote(train_x), y = quote(train_y),
-    metric = "AUC", tuneLength = 50,
-    trControl = train_controls)
+        return(model)
 
-fit_list <- map(names(model_list), function(m) {
+        }
+    )
 
-    registerDoParallel(cores = 8) # Use parallel processing
+    # Extract test Accuracies
+    accuracy_list <- map(fit_list, predict, test_x) %>%
+        map(~ . == test_y) %>%
+        map_dbl(mean, na.rm = TRUE)
 
-    print(m) # Update you on where things are running
+    names(accuracy_list) <- methods_list
+    accuracy_list
 
-    model_args <- c(global_args, method = m) # create a new argument object, use method here
-    model <- tryCatch(do.call(train, model_args), error = function(e) NULL)
+    # Extract `ready` probabilities by model
+    ready_probs <- map(fit_list, predict, test_x, type = "prob") %>%
+        map("ready")
 
-    write_rds(model, paste0("models/grade_8_", m, ".rds"))
+    names(ready_probs) <- methods_list
 
-    message("Model method: ", m, " completed.")
+    probs_df <- as_tibble(ready_probs) %>%
+        mutate(truth = test_y)
 
-    stopImplicitCluster()
+    # Extract test AUC
+    auc_list <- map_dbl(
+        .x = list(quote(gbm), quote(rpart), quote(rlda), quote(nnet), quote(xgbLinear), quote(xgbTree)),
+        .f = ~ roc_auc(probs_df, truth = truth, !!.)[['.estimate']]
+    )
 
-    return(model)
+    names(auc_list) <- methods_list
+    auc_list
 
-    }
-)
+    rm(list = ls())
 
-# Extract test Accuracies
-accuracy_list <- map(fit_list, predict, test_x) %>%
-    map(~ . == test_y) %>%
-    map_dbl(mean, na.rm = TRUE)
-
-names(accuracy_list) <- methods_list
-accuracy_list
-
-# Extract `ready` probabilities by model
-ready_probs <- map(fit_list, predict, test_x, type = "prob") %>%
-    map("ready")
-
-names(ready_probs) <- methods_list
-
-probs_df <- as_tibble(ready_probs) %>%
-    mutate(truth = test_y)
-
-# Extract test AUC
-auc_list <- map_dbl(
-    .x = list(quote(gbm), quote(rpart), quote(rlda), quote(nnet), quote(xgbLinear), quote(xgbTree)),
-    .f = ~ roc_auc(probs_df, truth = truth, !!.)
-)
-
-names(auc_list) <- methods_list
-auc_list
+}
--- R/2. predict.R
+++ R/2. predict.R
@@ -1,88 +1,81 @@
 library(tidyverse)
 library(caret)
 
-## Trained models
-model_gbm <- read_rds("models/grade_8_gbm.rds")
-model_rpart <- read_rds("models/grade_8_rpart.rds")
-model_rlda <- read_rds("models/grade_8_rlda.rds")
-model_nnet <- read_rds("models/grade_8_nnet.rds")
-model_xgblinear <- read_rds("models/grade_8_xgbLinear.rds")
-model_xgbtree <- read_rds("models/grade_8_xgbTree.rds")
 
-predictors <- c("n_absences", "enrollments", "E", "I", "R", "S", "assault", "weapons",
-    "theft_vandalism", "sexual_assault_harassment", "drugs_alcohol", "threat", "school_rules", "bullying", "fighting",
-    "scale_score_mt", "scale_score_mt_sq", "scale_score_rd", "scale_score_rd_sq",
-    "school_scale_score_mt", "school_scale_score_rd", "school_chronic_abs")
+for (grade in 6:8) {
 
-## Raw data to apply trained models to
-prediction_data_8 <- read_csv("data/prediction_data_8.csv",
-        col_types = "idciiiiiiiiiiiiiiiiiiiidddcciiiiiiiddddddd") %>%
-    filter(cohort == 2012,
-        !is.na(scale_score_mt) & !is.na(scale_score_rd) & !is.na(ready_grad)) %>%
-    mutate(system = if_else(system %in% c(790L, 791L), 792L, system),
-        system = case_when(
-            system == 792 & school %in% c(1L, 5L, 6L, 195L) ~ 793L,
-            system == 792 & school %in% c(3L, 7L, 20L, 25L, 30L, 33L, 90L, 95L, 150L, 155L, 170L) ~ 794L,
-            system == 792 & school %in% c(8L, 55L, 60L, 63L, 65L, 168L, 183L, 190L) ~ 795L,
-            system == 792 & school %in% c(70L, 100L, 109L, 111L, 160L) ~ 796L,
-            system == 792 & school == 116L ~ 797L,
-            system == 792 & school %in% c(78L, 123L, 130L, 133L) ~ 798L,
-            TRUE ~ system),
-        ready_grad = factor(if_else(ready_grad == 1, "ready", "not_ready")),
-        econ_dis = if_else(econ_dis == 1, "ED", "Non-ED"),
-        swd = if_else(swd == 1, "SWD", "Non-SWD"),
-        el = if_else(el == 1, "EL", "Non-EL")) %>%
-    select(system, school, ready_grad, one_of(predictors), gender, race, econ_dis, el, swd)
+    path <- paste0("data/prediction_data_", grade, ".csv")
+    
+    ## Trained models
+    models_list <- dir("models") %>%
+        grep(grade, ., value = TRUE) %>%
+        paste0("models/", .) %>%
+        map(read_rds)
+    
+    predictors <- c("n_absences", "enrollments", "E", "I", "R", "S", "assault", "weapons",
+        "theft_vandalism", "sexual_assault_harassment", "drugs_alcohol", "threat", "school_rules", "bullying", "fighting",
+        "scale_score_mt", "scale_score_mt_sq", "scale_score_rd", "scale_score_rd_sq",
+        "school_scale_score_mt", "school_scale_score_rd", "school_chronic_abs")
+    
+    ## Raw data to apply trained models to
+    prediction_data <- read_csv(path, col_types = "idciiiiiiiiiiiiiiiiiiiidddcciiiiiiiddddddd") %>%
+        filter(cohort == 2012,
+            !is.na(scale_score_mt) & !is.na(scale_score_rd) & !is.na(ready_grad)) %>%
+        mutate(system = if_else(system %in% c(790L, 791L), 792L, system),
+            system = case_when(
+                system == 792 & school %in% c(1L, 5L, 6L, 195L) ~ 793L,
+                system == 792 & school %in% c(3L, 7L, 20L, 25L, 30L, 33L, 90L, 95L, 150L, 155L, 170L) ~ 794L,
+                system == 792 & school %in% c(8L, 55L, 60L, 63L, 65L, 168L, 183L, 190L) ~ 795L,
+                system == 792 & school %in% c(70L, 100L, 109L, 111L, 160L) ~ 796L,
+                system == 792 & school == 116L ~ 797L,
+                system == 792 & school %in% c(78L, 123L, 130L, 133L) ~ 798L,
+                TRUE ~ system),
+            ready_grad = factor(if_else(ready_grad == 1, "ready", "not_ready")),
+            econ_dis = if_else(econ_dis == 1, "ED", "Non-ED"),
+            swd = if_else(swd == 1, "SWD", "Non-SWD"),
+            el = if_else(el == 1, "EL", "Non-EL")) %>%
+        select(system, school, ready_grad, one_of(predictors), gender, race, econ_dis, el, swd)
+    
+    # Preprocess by centering, scaling, and removing zero-variance predictors for consistency
+    # Models were trained on centered, scaled, and zv-removed data
+    train_preprocess <- prediction_data[predictors] %>%
+        preProcess(method = c("scale", "center", "zv"))
+    
+    prediction_data <- predict(train_preprocess, prediction_data)
+    
+    # Predictions and ready probabilities by model
+    preds <- map_dfc(models_list, predict, prediction_data)
+    names(preds) <- dir("models") %>% grep(grade, ., value = TRUE) %>% str_replace(".rds", "") %>% paste0("preds_", .)
+    
+    probs <- map(models_list, predict, prediction_data, type = "prob") %>%
+        map_dfc("ready")
+    names(probs) <- dir("models") %>% grep(grade, ., value = TRUE) %>% str_replace(".rds", "") %>% paste0("probs_", .)
+    
+    evaluation_data <- bind_cols(prediction_data, preds, probs)
+    
+    # Ensemble models by using each prediction as a predictor and using a lm
+    train_controls <- trainControl(
+        summaryFunction = twoClassSummary,  # look at classification stats
+        method = "repeatedcv",
+        repeats = 3,                        # repeated cv is pretty standard
+        savePredictions = "final",          # only need final model predictions
+        classProbs = TRUE)                  # want to generate class probabilities for AUC
+    
+    model_ensemble <- train(x = probs, y = evaluation_data$ready_grad,
+        method = "glm", family = "binomial", trControl = train_controls)
+    
+    model_stack <- train(x = probs, y = evaluation_data$ready_grad,
+        method = "gbm", trControl = train_controls)
+    
+    evaluation_data <- mutate(evaluation_data,
+            pred_ensemble = predict(model_ensemble, evaluation_data),
+            prob_ensemble = predict(model_ensemble, evaluation_data, type = "prob")$ready,
+            pred_stack = predict(model_stack, evaluation_data),
+            prob_stack = predict(model_stack, evaluation_data, type = "prob")$ready
+        )
+    
+    write_csv(evaluation_data, paste0("data/evaluation_data_", grade, ".csv", na = ""))
 
-# Preprocess by centering, scaling, and removing zero-variance predictors for consistency
-# Models were trained on centered, scaled, and zv-removed data
-train_preprocess <- prediction_data_8[predictors] %>%
-    preProcess(method = c("scale", "center", "zv"))
-
-prediction_data_8 <- predict(train_preprocess, prediction_data_8)
-
-# Predictions and ready probabilities by model
-evaluation_data_8 <- prediction_data_8 %>%
-    mutate(
-        pred_gbm = predict(model_gbm, prediction_data_8),
-        prob_gbm = predict(model_gbm, prediction_data_8, type = "prob")$ready,
-        pred_rpart = predict(model_rpart, prediction_data_8),
-        prob_rpart = predict(model_rpart, prediction_data_8, type = "prob")$ready,
-        pred_rlda = predict(model_rlda, prediction_data_8),
-        prob_rlda = predict(model_rlda, prediction_data_8, type = "prob")$ready,
-        pred_nnet = predict(model_nnet, prediction_data_8),
-        prob_nnet = predict(model_nnet, prediction_data_8, type = "prob")$ready,
-        pred_xgblinear = predict(model_xgblinear, prediction_data_8),
-        prob_xgblinear = predict(model_xgblinear, prediction_data_8, type = "prob")$ready,
-        pred_xgbtree = predict(model_xgbtree, prediction_data_8),
-        prob_xgbtree = predict(model_xgbtree, prediction_data_8, type = "prob")$ready,
-    # rlda is returning NaN for extremely small probabilities; replace these with the lowest observed predicted probability
-        prob_rlda = if_else(is.nan(prob_rlda), min(prob_rlda, na.rm = TRUE), prob_rlda)
-    )
-
-# Ensemble models by using each prediction as a predictor and using a lm
-train_controls <- trainControl(
-    summaryFunction = twoClassSummary,  # look at classification stats
-    method = "repeatedcv",
-    repeats = 3,                        # repeated cv is pretty standard
-    savePredictions = "final",          # only need final model predictions
-    classProbs = TRUE)                  # want to generate class probabilities for AUC
-
-probs <- c("prob_gbm", "prob_rpart", "prob_rlda", "prob_nnet", "prob_xgblinear", "prob_xgbtree")
-ensemble_x <- as.data.frame(evaluation_data_8[probs])
-ensemble_y <- evaluation_data_8$ready_grad
-
-model_ensemble <- train(x = ensemble_x, y = ensemble_y,
-    method = "glm", family = "binomial", trControl = train_controls)
-
-model_stack <- train(x = ensemble_x, y = ensemble_y,
-    method = "gbm", trControl = train_controls)
-
-evaluation_data_8 <- mutate(evaluation_data_8,
-        pred_ensemble = predict(model_ensemble, evaluation_data_8),
-        prob_ensemble = predict(model_ensemble, evaluation_data_8, type = "prob")$ready,
-        pred_stack = predict(model_stack, evaluation_data_8),
-        prob_stack = predict(model_stack, evaluation_data_8, type = "prob")$ready
-    )
-
-write_csv(evaluation_data_8, "data/evaluation_data_8.csv", na = "")
+    rm(list = ls())
+    
+}
